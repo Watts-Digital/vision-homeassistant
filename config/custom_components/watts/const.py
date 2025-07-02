a5740001f4a766d7f66b9876ab44d1d0b4f7e17d
TBR <theo.brilland@wattswater.com>
2025-07-02 16:00:19 +0200

    Re-order const

--- config/custom_components/watts/const.py
+++ config/custom_components/watts/const.py
@@ -16,16 +16,16 @@ OAUTH2_SCOPES = [
 # Update interval in seconds
 UPDATE_INTERVAL = 15
 
-# Default allowed setpoints range
-DEFAULT_MIN_SETPOINT = 5
-DEFAULT_MAX_SETPOINT = 35
-
 # API endpoints
 API_BASE_URL = "https://dev-vision.watts.io/api"
 
 # Interface types
 INTERFACE_THERMOSTAT = "action.devices.types.THERMOSTAT"
 INTERFACE_SWITCH = "action.devices.types.SWITCH"
+
+# Default allowed setpoints range
+DEFAULT_MIN_SETPOINT = 5
+DEFAULT_MAX_SETPOINT = 35
 
 # Mode mappings
 WATTS_MODE_TO_HVAC = {
